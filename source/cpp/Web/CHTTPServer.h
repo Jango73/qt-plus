ea3fcb529f2cc9e53a4b18080b7bd2de94083cb9
Guillaume Darier <guillaumed00@gmail.com>
2017-06-02 21:08:16 +0200

    Formatting

--- source/cpp/Web/CHTTPServer.h
+++ source/cpp/Web/CHTTPServer.h
@@ -23,67 +23,67 @@
 
 // First token in a HTTP header
 
-#define HTTP_HEADER						"HTTP/1.1 "
+#define HTTP_HEADER                     "HTTP/1.1 "
 
 #define HTTP_GET                        "GET"
 #define HTTP_POST                       "POST"
 
 // Possible error codes of a HTTP server
 
-#define HTTP_200_OK						"200 Ok"
+#define HTTP_200_OK                     "200 Ok"
 
-#define HTTP_201_CREATED				"201 Created"
-#define HTTP_202_ACCEPTED				"202 Accepted"
-#define HTTP_203_NA_INFORMATION			"203 Non-Authoritative Information"
-#define HTTP_204_NO_CONTENT				"204 No Content"
-#define HTTP_205_RESET_CONTENT			"205 Reset Content"
-#define HTTP_206_PARTIAL_CONTENT		"206 Partial Content"
+#define HTTP_201_CREATED                "201 Created"
+#define HTTP_202_ACCEPTED               "202 Accepted"
+#define HTTP_203_NA_INFORMATION         "203 Non-Authoritative Information"
+#define HTTP_204_NO_CONTENT             "204 No Content"
+#define HTTP_205_RESET_CONTENT          "205 Reset Content"
+#define HTTP_206_PARTIAL_CONTENT        "206 Partial Content"
 
-#define HTTP_400_BAD_REQUEST			"400 Bad Request"
-#define HTTP_401_UNAUTHORIZED			"401 Unauthorized"
-#define HTTP_402_PAYMENT_REQUIRED		"402 Payment Required"
-#define HTTP_403_FORBIDDEN				"403 Forbidden"
-#define HTTP_404_NOT_FOUND				"404 Not Found"
-#define HTTP_405_METHOD_NOT_ALLOWED		"405 Method Not Allowed"
-#define HTTP_406_NOT_ACCEPTABLE			"406 Not Acceptable"
-#define HTTP_407_PROXY_AUTH_REQUIRED	"407 Proxy Authentication Required"
-#define HTTP_408_REQUEST_TIMEOUT		"408 Request Timeout"
-#define HTTP_409_CONFLICT				"409 Conflict"
-#define HTTP_410_GONE					"410 Gone"
+#define HTTP_400_BAD_REQUEST            "400 Bad Request"
+#define HTTP_401_UNAUTHORIZED           "401 Unauthorized"
+#define HTTP_402_PAYMENT_REQUIRED       "402 Payment Required"
+#define HTTP_403_FORBIDDEN              "403 Forbidden"
+#define HTTP_404_NOT_FOUND              "404 Not Found"
+#define HTTP_405_METHOD_NOT_ALLOWED     "405 Method Not Allowed"
+#define HTTP_406_NOT_ACCEPTABLE         "406 Not Acceptable"
+#define HTTP_407_PROXY_AUTH_REQUIRED    "407 Proxy Authentication Required"
+#define HTTP_408_REQUEST_TIMEOUT        "408 Request Timeout"
+#define HTTP_409_CONFLICT               "409 Conflict"
+#define HTTP_410_GONE                   "410 Gone"
 
-#define HTTP_500_INTERNAL_SERVER_ERROR	"500 Internal Server Error"
-#define HTTP_501_NOT_IMPLEMENTED		"501 Not Implemented"
-#define HTTP_502_BAD_GATEWAY			"502 Bad Gateway"
-#define HTTP_503_SERVICE_UNAVAILABLE	"503 Service Unavailable"
-#define HTTP_504_GATEWAY_TIMEOUT		"504 Gateway Timeout"
-#define HTTP_505_HTTP_VERSION_NS		"505 HTTP Version Not Supported"
+#define HTTP_500_INTERNAL_SERVER_ERROR  "500 Internal Server Error"
+#define HTTP_501_NOT_IMPLEMENTED        "501 Not Implemented"
+#define HTTP_502_BAD_GATEWAY            "502 Bad Gateway"
+#define HTTP_503_SERVICE_UNAVAILABLE    "503 Service Unavailable"
+#define HTTP_504_GATEWAY_TIMEOUT        "504 Gateway Timeout"
+#define HTTP_505_HTTP_VERSION_NS        "505 HTTP Version Not Supported"
 
 // Important tokens in a HTTP header
 
-#define Token_Host						"Host:"
-#define Token_Connection				"Connection:"
-#define Token_ContentType				"Content-Type:"
-#define Token_ContentLength				"Content-Length:"
+#define Token_Host                      "Host:"
+#define Token_Connection                "Connection:"
+#define Token_ContentType               "Content-Type:"
+#define Token_ContentLength             "Content-Length:"
 #define Token_ContentDisposition        "Content-Disposition:"
 #define Token_boundary                  "boundary"
 #define Token_name                      "name"
 
 // MIME type tokens
 
-#define MIME_Content_PlainText			"text/plain"
-#define MIME_Content_HTML				"text/html"
-#define MIME_Content_CSS				"text/css"
-#define MIME_Content_URLForm			"application/x-www-form-urlencoded"
-#define MIME_Content_Javascript			"application/javascript"
-#define MIME_Content_Stream				"application/octet-stream"
-#define MIME_Content_PDF				"application/pdf"
-#define MIME_Content_XHTML				"application/xhtml+xml"
-#define MIME_Content_JSON				"application/json"
-#define MIME_Content_XML				"application/xml"
-#define MIME_Content_ZIP				"application/zip"
-#define MIME_Content_GIF				"image/gif"
-#define MIME_Content_JPG				"image/jpg"
-#define MIME_Content_PNG				"image/png"
+#define MIME_Content_PlainText          "text/plain"
+#define MIME_Content_HTML               "text/html"
+#define MIME_Content_CSS                "text/css"
+#define MIME_Content_URLForm            "application/x-www-form-urlencoded"
+#define MIME_Content_Javascript         "application/javascript"
+#define MIME_Content_Stream             "application/octet-stream"
+#define MIME_Content_PDF                "application/pdf"
+#define MIME_Content_XHTML              "application/xhtml+xml"
+#define MIME_Content_JSON               "application/json"
+#define MIME_Content_XML                "application/xml"
+#define MIME_Content_ZIP                "application/zip"
+#define MIME_Content_GIF                "image/gif"
+#define MIME_Content_JPG                "image/jpg"
+#define MIME_Content_PNG                "image/png"
 #define MIME_Content_MultiPart          "multipart"
 #define MIME_Content_MultiPart_FormData "multipart/form-data"
 #define MIME_Content_Custom             "custom"
@@ -243,11 +243,11 @@ public:
             }
         }
 
-        bool					m_bHeaderRead;
+        bool                    m_bHeaderRead;
         QString                 m_sMultipartBoundary;
-        int						m_iExpectedBytes;
-        QByteArray				m_baBuffer;
-        QMap<QString, QVariant>	m_vUserData;
+        int                     m_iExpectedBytes;
+        QByteArray              m_baBuffer;
+        QMap<QString, QVariant> m_vUserData;
     };
 
     //! This class executes requests in threaded mode
@@ -345,9 +345,9 @@ protected:
     QMutex                          m_mMutex;                   // Data protection
     int                             m_iRequestCount;            // Total request count
     int                             m_iMaxRequestPerSeconds;    //
-    bool                            m_bDisabled;				// Tells if the server should ignore requests
-    QVector<QString>                m_vAuthorizedFolders;		// Tells which folders users can access
-    QMap<QString, QString>          m_vExtensionToContentType;	// Used to converta file extension to a MIME type
+    bool                            m_bDisabled;                // Tells if the server should ignore requests
+    QVector<QString>                m_vAuthorizedFolders;       // Tells which folders users can access
+    QMap<QString, QString>          m_vExtensionToContentType;  // Used to converta file extension to a MIME type
     QThreadPool                     m_pProcessors;              // Threaded request processors
     QMap<QString, CRequestMonitor>  m_mMonitors;                // Anti-flooding monitor
     QStringList                     m_lStaticIPBlackList;
