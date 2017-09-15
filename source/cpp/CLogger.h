949dde321f36df0682533ab0cf79fea09f8a184c
Guillaume Darier <guillaume.darier@ext.mpsa.com>
2017-09-15 11:02:07 +0200

    Add debug facilities

--- source/cpp/CLogger.h
+++ source/cpp/CLogger.h
@@ -24,26 +24,33 @@
 // Use the following macros to log
 
 #ifdef LINUX
-#define __FILE_NOPATH__		(strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#define __FILE_NOPATH__     (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 #else
-#define __FILE_NOPATH__		(strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+#define __FILE_NOPATH__         (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
+#define __PRETTY_FUNCTION__     (__func__)
 #endif
 
 #ifndef NO_LOGGING
-#define LOG_DEBUG(a)			CLogger::getInstance()->log(llDebug,    a,__FILE_NOPATH__)
-#define LOG_INFO(a)				CLogger::getInstance()->log(llInfo,     a,__FILE_NOPATH__)
-#define LOG_WARNING(a)			CLogger::getInstance()->log(llWarning,  a,__FILE_NOPATH__)
-#define LOG_ERROR(a)			CLogger::getInstance()->log(llError,    a,__FILE_NOPATH__)
-#define LOG_CRITICAL(a)			CLogger::getInstance()->log(llCritical, a,__FILE_NOPATH__)
-#define LOG_ALWAYS(a)			CLogger::getInstance()->log(llAlways,   a,__FILE_NOPATH__)
-#define LOG_BUFFER(l,b,s)		CLogger::getInstance()->logBuffer (l,b,s)
+#define LOG_DEBUG(a)            CLogger::getInstance()->log(llDebug,    a, __FILE_NOPATH__)
+#define LOG_INFO(a)             CLogger::getInstance()->log(llInfo,     a, __FILE_NOPATH__)
+#define LOG_WARNING(a)          CLogger::getInstance()->log(llWarning,  a, __FILE_NOPATH__)
+#define LOG_ERROR(a)            CLogger::getInstance()->log(llError,    a, __FILE_NOPATH__)
+#define LOG_CRITICAL(a)         CLogger::getInstance()->log(llCritical, a, __FILE_NOPATH__)
+#define LOG_ALWAYS(a)           CLogger::getInstance()->log(llAlways,   a, __FILE_NOPATH__)
+#define LOG_BUFFER(l,b,s)       CLogger::getInstance()->logBuffer (l,b,s)
 
-#define LOG_DEBUG_TOKEN(a,b)	CLogger::getInstance()->log(llDebug,a,b)
-#define LOG_INFO_TOKEN(a,b)		CLogger::getInstance()->log(llInfo,a,b)
-#define LOG_WARNING_TOKEN(a,b)	CLogger::getInstance()->log(llWarning,a,b)
-#define LOG_ERROR_TOKEN(a,b)	CLogger::getInstance()->log(llError,a,b)
-#define LOG_CRITICAL_TOKEN(a)	CLogger::getInstance()->log(llCritical,a,b)
-#define LOG_ALWAYS_TOKEN(a,b)	CLogger::getInstance()->log(llAlways,a,b)
+#define LOG_DEBUG_TOKEN(a,b)    CLogger::getInstance()->log(llDebug,a,b)
+#define LOG_INFO_TOKEN(a,b)     CLogger::getInstance()->log(llInfo,a,b)
+#define LOG_WARNING_TOKEN(a,b)  CLogger::getInstance()->log(llWarning,a,b)
+#define LOG_ERROR_TOKEN(a,b)    CLogger::getInstance()->log(llError,a,b)
+#define LOG_CRITICAL_TOKEN(a)   CLogger::getInstance()->log(llCritical,a,b)
+#define LOG_ALWAYS_TOKEN(a,b)   CLogger::getInstance()->log(llAlways,a,b)
+
+#define LOG_METHOD_DEBUG(a)     LOG_DEBUG(QString("%1::%2() : %3").arg(typeid(*this).name()).arg(__PRETTY_FUNCTION__).arg(a))
+#define LOG_METHOD_INFO(a)      LOG_INFO(QString("%1::%2() : %3").arg(typeid(*this).name()).arg(__PRETTY_FUNCTION__).arg(a))
+#define LOG_METHOD_WARNING(a)   LOG_WARNING(QString("%1::%2() : %3").arg(typeid(*this).name()).arg(__PRETTY_FUNCTION__).arg(a))
+#define LOG_METHOD_ERROR(a)     LOG_ERROR(QString("%1::%2() : %3").arg(typeid(*this).name()).arg(__PRETTY_FUNCTION__).arg(a))
+#define LOG_METHOD_CRITICAL(a)  LOG_CRITICAL(QString("%1::%2() : %3").arg(typeid(*this).name()).arg(__PRETTY_FUNCTION__).arg(a))
 #else
 #define LOG_DEBUG(a)
 #define LOG_INFO(a)
@@ -59,7 +66,12 @@
 #define LOG_ERROR_TOKEN(a,b)
 #define LOG_CRITICAL_TOKEN(a,b)
 #define LOG_ALWAYS_TOKEN(a,b)
-#define LOG_CRITICAL_TOKEN(a,b)
+
+#define LOG_METHOD_DEBUG(a)
+#define LOG_METHOD_INFO(a)
+#define LOG_METHOD_WARNING(a)
+#define LOG_METHOD_ERROR(a)
+#define LOG_METHOD_CRITICAL(a)
 #endif
 
 //-------------------------------------------------------------------------------------------------
