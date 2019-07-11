859df07c06404fc8248d278837859e8d511c00dc
Guillaume Darier <guillaumed00@gmail.com>
2019-07-11 21:46:03 +0200

    Add a property macro

--- source/cpp/Macros.h
+++ source/cpp/Macros.h
@@ -30,3 +30,18 @@ public:                                                             \
     t g () const { return m_##p##s; }                               \
 protected:                                                          \
     t m_##p##s;
+
+// Declares a Qt property with setter and getter
+// Automatically adds the member variable
+// Does not implement the setter
+// Names the member variable as m_<prefix><set name> (for instance: m_iCount)
+// Parameters: Type, prefix for type, get name, set name
+#define Q_FAST_PROPERTY_NO_SET_IMPL(t, p, g, s)                     \
+public:                                                             \
+    Q_PROPERTY(t g READ g WRITE set##s NOTIFY g##Changed)           \
+public:                                                             \
+    t g () const { return m_##p##s; }                               \
+    void set##s (t value);                                          \
+    Q_SIGNAL void g##Changed();                                     \
+protected:                                                          \
+    t m_##p##s;
