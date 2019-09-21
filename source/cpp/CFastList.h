90c21afcc2d7a97af62bb7ea635445afe1663fe8
Guillaume Darier <guillaumed00@gmail.com>
2019-09-21 19:34:55 +0200

    Add a fast lookup list

---
+++ source/cpp/CFastList.h
@@ -0,0 +1,58 @@
+
+#pragma once
+
+// std
+#include <typeinfo>
+
+// Qt
+#include <QList>
+#include <QHash>
+
+//-------------------------------------------------------------------------------------------------
+
+//! Defines a fast lookup list
+template<typename K, typename T>
+class CFastList : public QList<T>
+{
+public:
+
+    CFastList()
+    {
+    }
+
+    void addItem(K key, const T& item, int index = -1)
+    {
+        if (index != -1)
+            QList<T>::insert(index, item);
+        else
+            QList<T>::append(item);
+
+        m_hLookup[key] = item;
+    }
+
+    void removeItem(K key)
+    {
+        QList<T>::removeAll(m_hLookup[key]);
+        m_hLookup.remove(key);
+    }
+
+    void removeItem(K key, const T& item)
+    {
+        removeAll(item);
+        m_hLookup.remove(key);
+    }
+
+    bool containsKey(K key)
+    {
+        return m_hLookup.contains(key);
+    }
+
+    T itemByKey(K key)
+    {
+        return m_hLookup[key];
+    }
+
+protected:
+
+    QHash<K, T> m_hLookup;
+};
